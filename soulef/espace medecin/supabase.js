c28898ec0834a75fbe085f940e5e6ec17791d9f9
souleflabiad13-max <souleflabiad13@gmail.com>
2026-04-18 17:47:41 +0630

    Delete soulef directory

--- soulef/espace medecin/supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const SUPABASE_URL = 'https://uuhdpvtnfaycqemhrelo.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_C-QCOapZ_QY92foN_Q-VRw_3WSoCos7';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
